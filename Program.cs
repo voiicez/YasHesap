05b012fa8a6e267cb34ca20e180ca8f9a3718278
behlul.sahin <behlul.sahin@ANK5-YZLMORT-02>
2022-08-15 13:44:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+//Nokta Atış Yaş Hesaplayıcı.
+Console.WriteLine("Doğum tarihinizi girin.(GG/AA/YYYY): ");
+DateTime dogumTarihi = DateTime.Parse(Console.ReadLine(), new CultureInfo("tr-TR"));
+DateTime bugun = DateTime.Today;
+TimeSpan fark = bugun - dogumTarihi;//bugun.subtract
+double yas= fark.TotalHours / (365 *24 +6);
+Console.WriteLine("Yaşınız: "+ (int)yas);
